8bfda6ee732434cb46223340f3adf4a33f5db93e
Deep6Lab <ting.xiao@unt.edu>
2022-01-03 13:35:29 -0600

    upload

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask import jsonify
+from flask import request
+import json
+import time
+import datetime
+from diarization import diarization
+from werkzeug.utils import secure_filename
+import os
+import asyncio
+
+UPLOAD_FOLDER = 'C:/Users/alish/OneDrive/Desktop/UniversityProjects/ResearchProject/ConvModAPI/uploads'
+ALLOWED_EXTENSIONS = {'wav','awb','m4a','mp3','mp4'}
+app = Flask(__name__)
+api = Api(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+class ConvModAPI(Resource):
+    def get(self):
+
+        try:
+            data = diarization().callSpeechAPI()
+
+
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...", ValueError)
+        return json.loads(data)
+
+    def post(self):
+        form = request.form
+        print(form.to_dict(self))
+
+        try:
+            if 'file' not in request.files:
+                return {"file":"Please add file"}, 400
+                # If the user does not select a file, the browser submits an
+                # empty file without a filename.
+            file = request.files['file']
+            if file.filename == '':
+                return {"file":"Please add file"}, 400
+
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                data = diarization().callSpeechAPI(filename, form.to_dict(self))
+                print(data)
+                #return redirect(url_for('download_file', name=filename))
+
+
+
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...", ValueError)
+        return {"data" : json.loads(data)},200
+
+api.add_resource(ConvModAPI, '/app')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ diarization.py
@@ -0,0 +1,58 @@
+import io
+import os
+import wave
+import json
+from google.cloud import speech_v1p1beta1 as speech
+from google.cloud import storage
+from google.cloud.speech_v1 import RecognizeResponse
+from google.protobuf.json_format import MessageToJson,MessageToDict
+
+class diarization():
+
+
+    def callSpeechAPI (self,filename,form) -> any:
+        pathJson = "resources/"
+        json_name = "client_secret.json"
+        pathSpeech = "uploads/"
+        pathSpeechFile = pathSpeech + filename
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = pathJson + json_name
+        client = speech.SpeechClient()
+        with open(pathSpeechFile, "rb") as audio_file:
+            content = audio_file.read()
+
+        audio = speech.RecognitionAudio(content=content)
+
+        diarization_config = speech.SpeakerDiarizationConfig(
+            enable_speaker_diarization=True,
+            min_speaker_count=2,
+            max_speaker_count=10,
+        )
+        if form["encoding"] == "AMR_WB":
+            config = speech.RecognitionConfig(
+                encoding=speech.RecognitionConfig.AudioEncoding.AMR_WB,
+                sample_rate_hertz=int(form["sampleRateHertz"]),
+                language_code="en-US",
+                diarization_config=diarization_config,
+            )
+        else:
+            config = speech.RecognitionConfig(
+                encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+                sample_rate_hertz=8000,
+                language_code="en-US",
+                diarization_config=diarization_config,
+            )
+
+        print("Waiting for operation to complete...")
+        response = client.recognize(config=config, audio=audio)
+        # result = response.results[-1]
+        #
+        # words_info = result.alternatives[0].words
+
+        # Printing out the output:
+        # for word_info in words_info:
+        #     print(
+        #         u"word: '{}', speaker_tag: {}".format(word_info.word, word_info.speaker_tag)
+        #     )
+        res = MessageToJson(response._pb, preserving_proto_field_name=True)
+        return res
+
